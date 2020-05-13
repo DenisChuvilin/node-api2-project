78f25723318479df2805f3483ffa713e152ec76f
DenisChuvilin <denischuv@gmail.com>
2020-05-12 20:45:00 -0700

    mvp met

--- routers/ExpressRouter.js
+++ routers/ExpressRouter.js
@@ -3,6 +3,40 @@ const router = express.Router();
 
 const db = require('../data/db');
 
+router.post('/', (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+  }
+  db.insert(req.body)
+    .then((post) => {
+      console.log(post);
+      res.status(201).json({ CREATED_POST: post });
+    })
+    .catch((error) => status(500).json({ error: 'There was an error while saving the post to the database' }));
+});
+
+router.post('/:id/comments', (req, res) => {
+  if (!req.body.text) {
+    res.status(400).json({ errorMessage: 'Please provide text for the comment.' });
+  } else {
+    db.findById(req.params.id)
+      .then((post) => {
+        if (post.length < 1) {
+          res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        } else {
+          db.insertComment(req.body)
+            .then((commentId) => {
+              console.log(commentId);
+
+              db.findCommentById(commentId.id).then((comment) => res.status(201).json({ CREATED: comment }));
+            })
+            .catch((err) => console.log(err.message));
+        }
+      })
+      .catch((err) => console.log(err.message));
+  }
+});
+
 router.get('/', (req, res) => {
   db.find()
     .then((posts) => res.status(200).json(posts))
@@ -27,39 +61,16 @@ router.get('/:id/comments', (req, res) => {
     .catch((error) => console.log(error.message));
 });
 
-router.post('/', (req, res) => {
-  db.insert(req.body)
-    .then((post) => res.status(201).json(post))
-    .catch((error) => console.log(error.message));
-});
-
-router.post('/:id/comments', (req, res) => {
-  console.log(req.params.id);
-  db.findById(req.params.id)
-
-    .then((post) => {
-      //   console.log(1, post);
-      if (post) {
-        // console.log(2, post);
-        if (!req.body.text) {
-          res.status(400).json({ errorMessage: 'Please provide text for the content' });
-        } else {
-          db.insertComment(req.body)
-            .then(({ id }) =>
-              db
-                .findCommentById(id)
-                .then((comment) => {
-                  res.status(201).json(comment);
-                })
-                .catch((err) => res.status(500).json(err.message))
-            )
-            .catch((error) => console.log(error.message));
-        }
+router.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then((removed) => {
+      if (removed === 1) {
+        res.status(200).json({ POST_DELETED_WITH_ID: req.params.id, NUMBER_OF_ITEMS_REMOVED: removed });
       } else {
-        res.status(404).json({ message: 'The post with the specified ID does not exist' });
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
       }
     })
-    .catch((error) => res.status(500).json({ error: 'last error message' }));
+    .catch((err) => res.status(500).json({ error: 'The post information could not be modified.' }));
 });
 
 router.put('/:id', (req, res) => {
@@ -73,18 +84,6 @@ router.put('/:id', (req, res) => {
         res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
       } else {
         db.update(req.params.id, req.body).then((updated) => res.status(201).json({ PUTTED: updated }));
-      }
-    })
-    .catch((err) => res.status(500).json({ error: 'The post information could not be modified.' }));
-});
-
-router.delete('/:id', (req, res) => {
-  db.remove(req.params.id)
-    .then((removed) => {
-      if (removed === 1) {
-        res.status(200).json({ POST_DELETED_WITH_ID: req.params.id, NUMBER_OF_ITEMS_REMOVED: removed });
-      } else {
-        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
       }
     })
     .catch((err) => res.status(500).json({ error: 'The post information could not be modified.' }));
